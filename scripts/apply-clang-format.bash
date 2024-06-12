f9e3c903faa1e148197ae16f007fb75f1f62182f
Marco Lampacrescia <marco.lampacrescia@de.bosch.com>
2024-06-12 17:05:04 +0200

    Prepare clang format file and run script
    Signed-off-by: Marco Lampacrescia <marco.lampacrescia@de.bosch.com>

---
+++ scripts/apply-clang-format.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get the path to the directory of script
+CODE_ROOT=$(dirname $(dirname $(realpath "${BASH_SOURCE[0]}")))
+# Move to the root of the project
+pushd $CODE_ROOT > /dev/null || exit
+# Apply clang-format to all source files
+clang-format --style=file -i src/**/*.cpp include/**/*.h* test/*.cpp
+# Return to the original directory
+popd > /dev/null || exit
