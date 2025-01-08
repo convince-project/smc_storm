9d677eafcc19ed1ab8f9e88baf030c997ddcbaa1
Marco Lampacrescia <marco.lampacrescia@de.bosch.com>
2025-01-08 13:21:04 +0100

    Use pre-commit, again
    Signed-off-by: Marco Lampacrescia <marco.lampacrescia@de.bosch.com>

--- scripts/apply-clang-style.bash
+++ scripts/apply-clang-style.bash
@@ -5,7 +5,7 @@ CODE_ROOT=$(dirname $(dirname $(realpath "${BASH_SOURCE[0]}")))
 # Move to the root of the project
 pushd $CODE_ROOT > /dev/null || exit
 # Apply clang-format to all source files
-clang-tidy-14 --config-file=.clang-tidy -p=build src/**/*.cpp src/*.cpp include/**/*.hpp test/**/*.cpp --fix-errors
-clang-format-14 --style=file -i src/**/*.cpp src/*.cpp include/**/*.hpp test/**/*.cpp
+clang-tidy --config-file=.clang-tidy -p=build src/**/*.cpp src/*.cpp include/**/*.hpp test/**/*.cpp --fix-errors
+clang-format --style=file -i src/**/*.cpp src/*.cpp include/**/*.hpp test/**/*.cpp
 # Return to the original directory
 popd > /dev/null || exit
