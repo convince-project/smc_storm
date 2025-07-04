bda66c9f5353c2ed0a3ebc4b9b3d89a50829839c
Marco Lampacrescia <marco.lampacrescia@de.bosch.com>
2025-07-04 09:10:15 +0200

    Refine tests
    Signed-off-by: Marco Lampacrescia <marco.lampacrescia@de.bosch.com>

--- test/unit_tests/test_statistical_methods.cpp
+++ test/unit_tests/test_statistical_methods.cpp
@@ -47,14 +47,16 @@ smc_storm::samples::BatchResults getSamplesBatch(
     return single_batch;
 }
 
-void runTest(const smc_storm::state_properties::PropertyType p_type, const std::string& stat_method, const double success_prob) {
+size_t runTest(const smc_storm::state_properties::PropertyType p_type, const std::string& stat_method, const double success_prob) {
     auto settings = getRequiredSettings(stat_method);
     smc_storm::samples::SamplingResults samples_holder(settings, p_type);
     bool always_fail = success_prob < FLT_EPSILON;
     size_t n_succ = 0u;
     size_t n_fail = 0u;
+    size_t n_non_terminating = 0u;
     size_t inverse_prob = always_fail ? std::numeric_limits<size_t>::max() : static_cast<size_t>(std::round(1.0 / success_prob));
     size_t n_batches = 0;
+    size_t total_non_terminating = 0u;
     while (samples_holder.newBatchNeeded(0u)) {
         if (always_fail || n_batches % inverse_prob != 0u) {
             n_succ = 0u;
@@ -63,84 +65,97 @@ void runTest(const smc_storm::state_properties::PropertyType p_type, const std::
             n_succ = 1u;
             n_fail = 0u;
         }
-        samples_holder.addBatchResults(getSamplesBatch(p_type, n_succ, n_fail, 0u), 0u);
+        n_non_terminating = (n_batches % 20 == 0) ? 1u : 0u;
+        samples_holder.addBatchResults(getSamplesBatch(p_type, n_succ, n_fail, n_non_terminating), 0u);
         n_batches++;
-    }
-    EXPECT_GT(n_batches, 100u);
-    if (!(always_fail || inverse_prob == 1U)) {
-        // In this case, we expect more samples
-        EXPECT_GT(n_batches, 600u);
+        total_non_terminating += n_non_terminating;
     }
     EXPECT_NEAR(samples_holder.getProbabilityVerifiedProperty(), success_prob, 0.001);
+    return n_batches;
 }
 
 TEST(SamplingResultsTest, ChernoffBoundProgress) {
     const auto p_type = smc_storm::state_properties::PropertyType::P;
-    auto settings = getRequiredSettings("chernoff");
-    smc_storm::samples::SamplingResults samples_holder(settings, p_type);
-    // Simulate some verified and not verified samples
-    for (size_t i = 0u; i < 20u; i++) {
-        auto batch = getSamplesBatch(p_type, 500u, 500u, 10u);
-        samples_holder.addBatchResults(batch, 0u);
-        ASSERT_EQ(samples_holder.newBatchNeeded(0u), i < 18u);
-    }
-    ASSERT_NEAR(samples_holder.getProbabilityVerifiedProperty(), 0.5, 0.001);
+    const std::string stat_method = "chernoff";
+    const size_t gen_batches = runTest(p_type, stat_method, 0.5);
+    ASSERT_GT(gen_batches, 10000u);
 }
 
 TEST(SamplingResultsTest, WaldBoundProgress) {
     const auto p_type = smc_storm::state_properties::PropertyType::P;
     const std::string stat_method = "wald";
-    runTest(p_type, stat_method, 1.0);
-    runTest(p_type, stat_method, 0.0);
-    runTest(p_type, stat_method, 0.25);
+    const size_t gen_batches_1 = runTest(p_type, stat_method, 1.0);
+    ASSERT_GT(gen_batches_1, 100u);
+    const size_t gen_batches_2 = runTest(p_type, stat_method, 0.0);
+    ASSERT_GT(gen_batches_2, 100u);
+    const size_t gen_batches_3 = runTest(p_type, stat_method, 0.25);
+    ASSERT_GT(gen_batches_3, 600u);
 }
 
 TEST(SamplingResultsTest, WilsonBoundProgress) {
     const auto p_type = smc_storm::state_properties::PropertyType::P;
     const std::string stat_method = "wilson";
-    runTest(p_type, stat_method, 1.0);
-    runTest(p_type, stat_method, 0.0);
-    runTest(p_type, stat_method, 0.25);
+    const size_t gen_batches_1 = runTest(p_type, stat_method, 1.0);
+    ASSERT_GT(gen_batches_1, 100u);
+    const size_t gen_batches_2 = runTest(p_type, stat_method, 0.0);
+    ASSERT_GT(gen_batches_2, 100u);
+    const size_t gen_batches_3 = runTest(p_type, stat_method, 0.25);
+    ASSERT_GT(gen_batches_3, 600u);
 }
 
 TEST(SamplingResultsTest, WilsonCorrectedBoundProgress) {
     const auto p_type = smc_storm::state_properties::PropertyType::P;
     const std::string stat_method = "wilson_corrected";
-    runTest(p_type, stat_method, 1.0);
-    runTest(p_type, stat_method, 0.0);
-    runTest(p_type, stat_method, 0.25);
+    const size_t gen_batches_1 = runTest(p_type, stat_method, 1.0);
+    ASSERT_GT(gen_batches_1, 100u);
+    const size_t gen_batches_2 = runTest(p_type, stat_method, 0.0);
+    ASSERT_GT(gen_batches_2, 100u);
+    const size_t gen_batches_3 = runTest(p_type, stat_method, 0.25);
+    ASSERT_GT(gen_batches_3, 600u);
 }
 
 TEST(SamplingResultsTest, ClopperPearsonBoundProgress) {
     const auto p_type = smc_storm::state_properties::PropertyType::P;
     const std::string stat_method = "clopper_pearson";
-    runTest(p_type, stat_method, 1.0);
-    runTest(p_type, stat_method, 0.0);
-    runTest(p_type, stat_method, 0.25);
+    const size_t gen_batches_1 = runTest(p_type, stat_method, 1.0);
+    ASSERT_GT(gen_batches_1, 100u);
+    const size_t gen_batches_2 = runTest(p_type, stat_method, 0.0);
+    ASSERT_GT(gen_batches_2, 100u);
+    const size_t gen_batches_3 = runTest(p_type, stat_method, 0.25);
+    ASSERT_GT(gen_batches_3, 600u);
 }
 
 TEST(SamplingResultsTest, AdaptiveBoundProgress) {
     const auto p_type = smc_storm::state_properties::PropertyType::P;
     const std::string stat_method = "adaptive";
-    runTest(p_type, stat_method, 1.0);
-    runTest(p_type, stat_method, 0.0);
-    runTest(p_type, stat_method, 0.25);
+    const size_t gen_batches_1 = runTest(p_type, stat_method, 1.0);
+    ASSERT_GT(gen_batches_1, 400u);
+    const size_t gen_batches_2 = runTest(p_type, stat_method, 0.0);
+    ASSERT_GT(gen_batches_2, 400u);
+    const size_t gen_batches_3 = runTest(p_type, stat_method, 0.25);
+    ASSERT_GT(gen_batches_3, 600u);
 }
 
 TEST(SamplingResultsTest, ArcsineBoundProgress) {
     const auto p_type = smc_storm::state_properties::PropertyType::P;
     const std::string stat_method = "arcsine";
-    runTest(p_type, stat_method, 1.0);
-    runTest(p_type, stat_method, 0.0);
-    runTest(p_type, stat_method, 0.25);
+    const size_t gen_batches_1 = runTest(p_type, stat_method, 1.0);
+    ASSERT_GT(gen_batches_1, 100u);
+    const size_t gen_batches_2 = runTest(p_type, stat_method, 0.0);
+    ASSERT_GT(gen_batches_2, 100u);
+    const size_t gen_batches_3 = runTest(p_type, stat_method, 0.25);
+    ASSERT_GT(gen_batches_3, 600u);
 }
 
 TEST(SamplingResultsTest, ChowRobbinsBoundProgress) {
     const auto p_type = smc_storm::state_properties::PropertyType::P;
     const std::string stat_method = "chow_robbins";
-    runTest(p_type, stat_method, 1.0);
-    runTest(p_type, stat_method, 0.0);
-    runTest(p_type, stat_method, 0.25);
+    const size_t gen_batches_1 = runTest(p_type, stat_method, 1.0);
+    ASSERT_GT(gen_batches_1, 100u);
+    const size_t gen_batches_2 = runTest(p_type, stat_method, 0.0);
+    ASSERT_GT(gen_batches_2, 100u);
+    const size_t gen_batches_3 = runTest(p_type, stat_method, 0.25);
+    ASSERT_GT(gen_batches_3, 600u);
 }
 
 int main(int argc, char** argv) {
