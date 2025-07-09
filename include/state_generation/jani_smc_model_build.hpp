d6c3f1a47c319281ece9e346ad2015b626306d17
Marco Lampacrescia <marco.lampacrescia@de.bosch.com>
2025-07-09 13:48:34 +0200

    First plugin-less version of new built model
    Signed-off-by: Marco Lampacrescia <marco.lampacrescia@de.bosch.com>

---
+++ include/state_generation/jani_smc_model_build.hpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2025 Robert Bosch GmbH and its subsidiaries
+ *
+ * This file is part of smc_storm.
+ *
+ * smc_storm is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * smc_storm is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with smc_storm.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "model_checker/smc_plugin_instance.hpp"
+#include "state_generation/available_actions.hpp"
+#include "state_properties/state_variable_information.hpp"
+#include <optional>
+#include <random>
+#include <smc_verifiable_plugins/smc_plugin_base.hpp>
+#include <stdint.h>
+#include <storm/builder/RewardModelInformation.h>
+#include <storm/generator/CompressedState.h>
+#include <storm/generator/TransientVariableInformation.h>
+#include <storm/generator/VariableInformation.h>
+#include <storm/storage/expressions/ExpressionEvaluator.h>
+#include <storm/storage/jani/Model.h>
+#include <storm/utility/constants.h>
+#include <storm/utility/ConstantsComparator.h>
+#include <unordered_map>
+
+namespace smc_storm::state_generation {
+template <typename ValueType>
+class JaniSmcModelBuild {
+  public:
+    static constexpr uint64_t SILENT_ACTION_ID = std::numeric_limits<uint64_t>::max();
+    // A location could have multiple possible edges
+    using LocationEdges = std::vector<std::reference_wrapper<const storm::jani::Edge>>;
+    // Each entry in the vector relates to a location (0 to n)
+    using AutomatonAction = std::vector<LocationEdges>;
+    // All actions belonging to an automaton
+    using AutomatonActionsSet = std::vector<AutomatonAction>;
+    // A map from the automaton ID to the related action ID to take
+    using AutomatonToActionId = std::pair<uint64_t, uint64_t>;
+    // A composite edge is a map from action_id to a set of automata with their related action ID
+    using CompositeEdge = std::pair<uint64_t, std::vector<AutomatonToActionId>>;
+    /*!
+     * @brief Build an internal model from the provided JANI object.
+     * @param jani_model The model we are building from.
+     */
+    JaniSmcModelBuild(const storm::jani::Model& jani_model);
+
+    // Getters need to be done
+  private:
+    // All automata in the system
+    std::vector<std::reference_wrapper<const storm::jani::Automaton>> _jani_automata;
+    // Vector of Composite Edges, each one with an ID and the related Automaton to ActionID map
+    std::vector<CompositeEdge> _composite_edges;
+    // Vector of automata, each one having an array of (automaton's) actions.
+    std::vector<AutomatonActionsSet> _automata_actions;
+};
+}  // namespace smc_storm::state_generation
---
+++ src/state_generation/jani_smc_model_build.cpp
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2025 Robert Bosch GmbH and its subsidiaries
+ *
+ * This file is part of smc_storm.
+ *
+ * smc_storm is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * smc_storm is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with smc_storm.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <storm/exceptions/InvalidModelException.h>
+#include <storm/storage/jani/AutomatonComposition.h>
+#include <storm/storage/jani/ParallelComposition.h>
+
+#include "state_generation/jani_smc_model_build.hpp"
+
+namespace smc_storm::state_generation {
+template <typename ValueType>
+JaniSmcModelBuild<ValueType>::JaniSmcModelBuild(const storm::jani::Model& jani_model) {
+    const auto& jani_composition = jani_model.getSystemComposition();
+    if (jani_composition.isAutomatonComposition()) {
+        // We can treat it as a single automaton
+        const uint64_t automaton_idx = 0u;
+        _jani_automata.reserve(1u);
+        _automata_actions.reserve(1u);
+        _composite_edges.reserve(1u);
+        STORM_LOG_THROW(
+            jani_model.getAutomata().size() == 1u, storm::exceptions::InvalidModelException, "Only one automaton expected here.");
+        const storm::jani::Automaton& single_automaton = jani_model.getAutomata().front();
+        _jani_automata.emplace_back(std::ref(single_automaton));
+        // TODO: Plugins
+        // computeAutomatonPluginAssociation(automaton, automaton_index);
+        _automata_actions.emplace_back(AutomatonActionsSet({AutomatonAction(single_automaton.getNumberOfLocations(), LocationEdges())}));
+        for (const storm::jani::Edge& aut_edge : single_automaton.getEdges()) {
+            // Access the 0th automaton and its 0th action and append the edge to the related location
+            _automata_actions.at(0u).at(0u).at(aut_edge.getSourceLocationIndex()).emplace_back(std::ref(aut_edge));
+        }
+        _composite_edges.emplace_back(SILENT_ACTION_ID, std::vector<AutomatonToActionId>({AutomatonToActionId(automaton_idx, 0U)}));
+    } else {
+        STORM_LOG_THROW(jani_composition.isParallelComposition(), storm::exceptions::InvalidModelException, "Unexpected model type found.");
+        // This is the typical configuration, with the network of synched automata
+        const storm::jani::ParallelComposition& parallel_composition = jani_composition.asParallelComposition();
+        const uint64_t n_sub_automata = parallel_composition.getNumberOfSubcompositions();
+        _jani_automata.reserve(n_sub_automata);
+        _automata_actions.reserve(n_sub_automata);
+        for (const auto& sub_component : parallel_composition.getSubcompositions()) {
+            STORM_LOG_THROW(
+                sub_component->isAutomatonComposition(), storm::exceptions::InvalidModelException, "Expected flat parallel composition.");
+            const storm::jani::Automaton& sub_automaton =
+                jani_model.getAutomaton(sub_component->asAutomatonComposition().getAutomatonName());
+            const uint64_t sub_automaton_idx = _jani_automata.size();
+            // Add an entry for the current automaton
+            _jani_automata.emplace_back(std::ref(sub_automaton));
+            _automata_actions.emplace_back();
+            // TODO: Plugins
+            // computeAutomatonPluginAssociation(automaton, automaton_index);
+            // Compute the silent actions first (only related to this automaton)
+            AutomatonAction sub_aut_silent_action(sub_automaton.getNumberOfLocations(), LocationEdges());
+            bool has_silent_edges = false;
+            for (const storm::jani::Edge& sub_aut_edge : sub_automaton.getEdges()) {
+                if (sub_aut_edge.hasSilentAction()) {
+                    sub_aut_silent_action.at(sub_aut_edge.getSourceLocationIndex()).emplace_back(std::ref(sub_aut_edge));
+                    has_silent_edges = true;
+                }
+            }
+            if (has_silent_edges) {
+                _automata_actions.at(sub_automaton_idx).emplace_back(std::move(sub_aut_silent_action));
+                STORM_LOG_THROW(
+                    _automata_actions.at(sub_automaton_idx).size() == 1u, storm::exceptions::UnexpectedException,
+                    "Unexpected size of automaton actions.");
+                _composite_edges.emplace_back(
+                    SILENT_ACTION_ID, std::vector<AutomatonToActionId>({AutomatonToActionId(sub_automaton_idx, 0U)}));
+            }
+        }
+        // Then compute the synched actions
+        for (const auto& synched_action : parallel_composition.getSynchronizationVectors()) {
+            const std::string& composed_action_name = synched_action.getOutput();
+            uint64_t composed_action_index = jani_model.getActionIndex(composed_action_name);
+            CompositeEdge composition_instance = {composed_action_index, {}};
+            for (uint64_t sub_aut_idx = 0u; sub_aut_idx < n_sub_automata; sub_aut_idx++) {
+                const std::string& sub_action_name = synched_action.getInput(sub_aut_idx);
+                if (!storm::jani::SynchronizationVector::isNoActionInput(sub_action_name)) {
+                    const uint64_t jani_action_id = jani_model.getActionIndex(sub_action_name);
+                    const storm::jani::Automaton sub_automaton = _jani_automata.at(sub_aut_idx).get();
+                    AutomatonAction sub_aut_action(sub_automaton.getNumberOfLocations(), LocationEdges());
+                    const uint64_t sub_aut_action_id = _automata_actions.at(sub_aut_idx).size();
+                    bool at_least_one_edge = false;
+                    for (const storm::jani::Edge& edge : sub_automaton.getEdges()) {
+                        if (edge.getActionIndex() == jani_action_id) {
+                            at_least_one_edge = true;
+                            sub_aut_action.at(edge.getSourceLocationIndex()).emplace_back(std::ref(edge));
+                        }
+                    }
+                    STORM_LOG_THROW(at_least_one_edge, storm::exceptions::InvalidModelException, "Found always invalid edge.");
+                    _automata_actions.at(sub_aut_idx).emplace_back(std::move(sub_aut_action));
+                    composition_instance.second.emplace_back(sub_aut_idx, sub_aut_action_id);
+                }
+            }
+            _composite_edges.emplace_back(std::move(composition_instance));
+        }
+    }
+}
+
+template class JaniSmcModelBuild<double>;
+}  // namespace smc_storm::state_generation
